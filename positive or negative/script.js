91a8e6ae6ec6c15b603b97c4a4285871635c205f
Noshin Tabassum <81520766+noshintabassum@users.noreply.github.com>
2022-03-11 12:53:24 +0600

    Add files via upload

---
+++ positive or negative/script.js
@@ -0,0 +1,5 @@
+var num = prompt("enter your number : ");
+
+//using ternary operator ....
+
+(num < 0) ? document.write("type of number " + num + " : negative"): document.write("type of number " + num + " : positive");
